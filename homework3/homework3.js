122ad7116a1d60d8d7253a29667c47e8c576522e
Liza-Sim <elizabet96@ukr.net>
2023-01-19 23:22:43 +0200

    Average

---
+++ homework3/homework3.js
@@ -0,0 +1,8 @@
+let numberOne = prompt('Привіт! Введи першу цифру!');
+let numberTwo = prompt('Чудово! Тепер вводь другу!');
+let numberThree = prompt('І тепер ще одну, останню!');
+let arithmeticalMean = (+numberOne + +numberTwo + +numberThree) / 3;
+alert(`Середнє арифметичне ${arithmeticalMean}`);
+
+
+
